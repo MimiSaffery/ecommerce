3f62e70f5ade2df5eff38e019b19bf161c19a2b7
mimiscarlett <68610443+mimiscarlett@users.noreply.github.com>
2021-05-07 15:44:03 +0100

    Name change

--- studio/src/deskStructure/index.js
+++ studio/src/deskStructure/index.js
@@ -27,7 +27,7 @@ const hiddenDocTypes = (listItem) =>
 
 export default () =>
   S.list()
-    .title("Pulp Inc.")
+    .title("Jacob clothes.")
     .items([
       S.documentTypeListItem("product").title("Products"),
       S.listItem()
